0530cfdd5166e474e38b13e341f65389ac414852
Brian Xiao <brianxiao7@gmail.com>
2026-08-26 17:09:24 -0400

    fair_benchmark_h100.sh: use /home voxtell env, export HF_HOME

--- fair_benchmark_h100.sh
+++ fair_benchmark_h100.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=0:15:00
 #SBATCH --account=rrg-jma
 
-source /scratch/brianx7/envs/nninteractive/bin/activate
+source /home/brianx7/envs/voxtell/bin/activate
 
 export TORCH_HOME=/scratch/brianx7/torch_home
 export XDG_CACHE_HOME=/scratch/brianx7/cache
